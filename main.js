692bdb8d2bc87f8e7bd80e594fb74c9b99408010
TeerthDholariya <127885197+TeerthDholariya@users.noreply.github.com>
2024-05-06 17:35:37 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+//https://teachablemachine.withgoogle.com/models/8NqG5c9Ok/
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/8NqG5c9Ok/model.json', modelReady);
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
